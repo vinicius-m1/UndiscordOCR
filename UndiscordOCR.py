f133abecc49b1840a05b30905e5dfc32323a3a8c
Vinicius M <github@viniciusm.anonaddy.com>
2024-02-12 00:33:04 -0300

    Code optimizations

--- UndiscordOCR.py
+++ UndiscordOCR.py
@@ -1,16 +1,20 @@
 #https://github.com/vinicius-m1/UndiscordOCR.git
 import discord
 from PIL import Image
+from PIL import ImageFile
 import pytesseract
+#import sys
 import requests
 import os.path
 import asyncio
 import shutil
+from io import BytesIO
 import pickle
 from configparser import ConfigParser
 from datetime import datetime
 intents = discord.Intents.default()
 intents.message_content = True
+ImageFile.LOAD_TRUNCATED_IMAGES = True 
 
 
 
@@ -23,6 +27,7 @@ config = ConfigParser()
 config.read("config.ini")
 config_data = config['default']
 token = config_data['token']
+lang_ocr = config_data['lang_ocr']
 channel_id = int(config_data['channel_id'])
 history_deletion = config_data['history_deletion']
 store_flagged = config_data['store_flagged_messeges']
@@ -56,21 +61,29 @@ def load_from_file():
          return(pickle.load(handle))
 
 
+async def OCR(data):
+    return (pytesseract.image_to_string(Image.open(BytesIO(data)),lang=lang_ocr, timeout=100)) #OCR 
+
+
 async def delete_history():
+
+
+    if not os.path.isfile('last_message.pickle'):
+        print ("Creating save file for last message.")
+        open('last_message.pickle', 'a').close()
+        save_to_file(datetime.now())
     
     print (f"BLOCKED WORDS: {word}")
     channel = client.get_channel(channel_id)
     i =0
-    #last_message = None
-    #save_to_file(last_message)
-    instances =0
-    instances = instances + 1
+
     while True:
         last_message = load_from_file();
         async for message in channel.history(limit = 100, before=last_message):
         
-            i = i+1
+            #i = i+1
             last_message = message.created_at
+            
             await asyncio.sleep(0) #testing
             if message.attachments:
             
@@ -82,38 +95,46 @@ async def delete_history():
                 if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".webp"):
 
                     try:
-                        filename = (str(message.created_at) + filename) # make filename more unique
-                        if (len(filename) >= 100): filename = filename[(len(filename)-10):] # trim if filename too big
-                        await asyncio.sleep(0) #testing
-                        fileLocation = os.path.join('./images', filename)
-                        img_data = requests.get(url).content
-                                
-                        open(fileLocation, 'wb').write(img_data) 
+                        img_data = requests.get(url).content                             
                         await asyncio.sleep(0) #testing
                     except Exception as err: 
-                        print(bcolors.ENDC,"Error saving image",bcolors.ENDC, err) 
+                        print(bcolors.FAIL,"Error getting content!",bcolors.ENDC, err) 
                         continue
             
                     try:
                         await asyncio.sleep(0) #testing
-                        text = pytesseract.image_to_string(fileLocation) #OCR
-                    except Exception as err: 
-                        print(bcolors.WARNING, "OCR Error, probably attachment not an image:",bcolors.ENDC,filename, err)
-                        os.remove(fileLocation)
+                        text = await OCR(img_data) #OCR 
+                        #print(text)
+                    except RuntimeError as timeout_error: 
+                        print(bcolors.FAIL, "OCR Error:",bcolors.ENDC,filename, timeout_error)
+                        #os.execv(sys.executable, ['python3'] + sys.argv) #restarts script
+                        #os._exit(0); #break point
                         continue            
 
                     
                     if words_in_string(word, text):
-                        await asyncio.sleep(0) #testing
+                        
+                        if store_flagged == 'True':
+                            filename = (str(message.created_at) + filename) # make filename more unique
+                            if (len(filename) >= 100): filename = filename[(len(filename)-10):] # trim if filename too big
+                            await asyncio.sleep(0) #testing
+                            fileLocation = os.path.join('./images/flagged', filename)
+                            with open(fileLocation, 'wb') as img:
+                                img.write(img_data)
+                                
+
                         print(bcolors.WARNING, "Keyword found in OCR reading! Deleting chat message.", bcolors.ENDC)
                         print (text)
                         await message.delete()
-                        shutil.move( fileLocation,'./images/flagged')
-                    else: os.remove(fileLocation)
+                        
+                        
+                    
                     await asyncio.sleep(0) #testing
+        i = i+100
+        print(f"{i} messages searched until now. last message was: {last_message}")
         
-        print(f"{i} messages searched until now. inst:{instances} last message was: {last_message}")
         save_to_file(last_message)
+        
 
 
 #WATCH FOR MESSAGES IN REAL TIME
